4e06706582b2a2c40c198b1702156740aa624bdb
Yuv <billiondollarone07@gmail.com>
2019-04-29 19:53:27 +0530

    Kotlin test

--- hello.kt
+++ hello.kt
@@ -1,3 +1,19 @@
 fun main(args: Array<String>) {
-    println("Hello, World!")
+   val a: Int = 10000
+   val d: Double = 100.00
+   val f: Float = 100.00f
+   val l: Long = 1000000004
+   val s: Short = 10
+   val z = "some string"
+   val y = 'c'
+   val b: Boolean = true
+   val arr: IntArray = intArrayOf(1, 2, 3, 4, 5, 6)
+   
+   println("Your Int Value is "+a+" Your Double  Value is "+d+" Your Float Value is "+f);
+   println("Your Long Value is "+l);
+   println("Your Short Value is "+s);
+   println("Your Byte Value is "+z);
+   println("The character is "+y);
+   println("The statement is "+b);
+   println("Display \n"+arr[0]);
 }
